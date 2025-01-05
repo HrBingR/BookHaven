fee429894db94442a12fea51925d0e164e2d0e51
anthonysmillie <anthony@everlytic.com>
2025-01-05 04:21:49 +0200

    Added additional react routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Home from './components/Home';
 import Authors from './components/Authors';
+import AuthorPage from './components/AuthorPage';
+import Reader from "./components/Reader.tsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -12,10 +14,12 @@ const App: React.FC = () => {
     <Router>
       <div className="d-flex">
         <Sidebar />
-        <div className="flex-grow-1">
+        <div className="flex-grow-1 d-flex flex-column">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/authors" element={<Authors />} /> {/* Add the Authors route */}
+            <Route path="/authors/:authorName" element={<AuthorPage />} />
+            <Route path="/read/:identifier" element={<Reader />} />
           </Routes>
         </div>
       </div>
