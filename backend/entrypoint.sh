eb97253a3bbd1a9b849f84b20ac6f7f9a327f602
anthonysmillie <anthony@everlytic.com>
2025-01-05 17:09:18 +0200

    Added new config to specify gunicorn workers

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 APP_PORT=${PORT:-5000}
+NUM_WORKERS=${WORKERS:-1}
 
 # Run migrations
 echo "Checking and applying database migrations..."
 python migrations.py
 
 # Start the application
-exec gunicorn -w 4 -b 0.0.0.0:$APP_PORT main:app
+exec gunicorn -w $NUM_WORKERS -b 0.0.0.0:$APP_PORT main:app
