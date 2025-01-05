ad2d613322d57ac3fc979bf656a448d8bd80b0c5
anthonysmillie <anthony@everlytic.com>
2025-01-05 04:28:15 +0200

    Added config to work with any DB engine based on DB_TYPE config option

--- backend/functions/db.py
+++ backend/functions/db.py
@@ -1,13 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.epub_metadata import Base
+from config.config import config
 
-DATABASE_URL = 'sqlite:///epub_library.db'
+# Dynamically construct the database URL
+DATABASE_URL = config.get_database_url()
+
 engine = create_engine(DATABASE_URL)
 
+
 def init_db():
+    """
+    Initializes the database by creating necessary tables.
+    """
     Base.metadata.create_all(engine)
 
+
 def get_session():
+    """
+    Provides a new database session (connection).
+    """
     Session = sessionmaker(bind=engine)
     return Session()
