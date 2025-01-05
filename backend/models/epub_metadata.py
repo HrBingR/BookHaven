573394fa21b243205204b17e2894edb14706abb3
anthonysmillie <anthony@everlytic.com>
2025-01-05 04:28:54 +0200

    Updated model to include progress information

--- backend/models/epub_metadata.py
+++ backend/models/epub_metadata.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.mysql import LONGBLOB
 
 Base = declarative_base()
 
@@ -7,11 +8,12 @@ class EpubMetadata(Base):
     __tablename__ = 'epub_metadata'
 
     id = Column(Integer, primary_key=True)
-    identifier = Column(String, unique=True, nullable=False) # Add 'index=True'
-    title = Column(String)
-    authors = Column(String)
-    series = Column(String)
+    identifier = Column(String(255), unique=True, nullable=False) # Add 'index=True'
+    title = Column(String(255))
+    authors = Column(String(255))
+    series = Column(String(255))
     seriesindex = Column(Float)
-    relative_path = Column(String, unique=True)
-    cover_image_data = Column(LargeBinary)
-    cover_media_type = Column(String)
+    relative_path = Column(String(255), unique=True)
+    cover_image_data = Column(LargeBinary().with_variant(LONGBLOB, 'mysql'))
+    cover_media_type = Column(String(255))
+    progress = Column(String(255), nullable=True)
