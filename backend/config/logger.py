5723bd523470598e1befa6332ef54af680873d3d
anthonysmillie <anthony@everlytic.com>
2025-01-19 20:29:16 +0200

    Logger has been restructured to allow for additional arguments for more flexibility, and better error and exception logging

--- backend/config/logger.py
+++ backend/config/logger.py
@@ -1,25 +1,41 @@
 import logging
-from .config import config
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO').upper()
+valid_levels = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL']
+if LOG_LEVEL not in valid_levels:
+    print(f"Invalid log level '{LOG_LEVEL}', defaulting to INFO")
+    LOG_LEVEL = "INFO"
 
 class Logger:
     def __init__(self):
         self.logger = logging.getLogger('epubdl')
-        self.logger.setLevel(getattr(logging, config.LOG_LEVEL))
-        self.handler = logging.StreamHandler()
-        self.log_formatter = logging.Formatter(
-            fmt="time=%(asctime)s level=%(levelname)s msg=\"%(message)s\"",
-            datefmt="%Y-%m-%dT%H:%M:%S"
-        )
-        self.handler.setFormatter(self.log_formatter)
-        self.handler.setLevel(getattr(logging, config.LOG_LEVEL))
-        self.logger.addHandler(self.handler)
-    def info(self, msg):
-        self.logger.info(msg)
-    def debug(self, msg):
-        self.logger.debug(msg)
-    def error(self, msg):
-        self.logger.error(msg)
-    def warning(self, msg):
-        self.logger.warning(msg)
+        self.logger.propagate = True
+        if not self.logger.handlers:
+            self.logger.setLevel(getattr(logging, LOG_LEVEL))
+            self.handler = logging.StreamHandler()
+            self.log_formatter = logging.Formatter(
+                fmt="time=%(asctime)s level=%(levelname)s msg=\"%(message)s\"",
+                datefmt="%Y-%m-%dT%H:%M:%S"
+            )
+            self.handler.setFormatter(self.log_formatter)
+            self.handler.setLevel(getattr(logging, LOG_LEVEL))
+            self.logger.addHandler(self.handler)
+    def info(self, msg, *args):
+        self.logger.info(msg, *args)
+    def debug(self, msg, *args):
+        self.logger.debug(msg, *args)
+    def error(self, msg, *args, exc_info=None):
+        if isinstance(msg, Exception):
+            self.logger.error(str(msg), exc_info=msg)
+        else:
+            self.logger.error(msg, *args, exc_info=exc_info)
+    def warning(self, msg, *args):
+        self.logger.warning(msg, *args)
+    def exception(self, msg, *args):
+        self.logger.exception(msg, *args)
 
 logger = Logger()
