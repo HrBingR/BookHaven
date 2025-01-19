8195264da565408ca86f4f531e6f92315e0866c0
anthonysmillie <anthony@everlytic.com>
2025-01-20 01:00:18 +0200

    Updated CORS configuration with new routes

--- backend/functions/extensions.py
+++ backend/functions/extensions.py
@@ -1,6 +1,7 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_cors import CORS
+from config.config import config
 
 limiter = Limiter(
     key_func=get_remote_address,
@@ -12,9 +13,12 @@ def setup_limiter(app):
     return limiter
 
 def setup_cors(app):
+    allowed_origin = config.BASE_URL
     CORS(app, resources={
-    r"/api/*": {"origins": "*"},  # Allow all origins for API routes
-    r"/stream/*": {"origins": "*"},  # Allow all origins for streaming routes
-    r"/files/*": {"origins": "*"},  # Allow all origins for file-serving routes
-    r"/download/*": {"origins": "*"},  # Allow all origins for file-serving routes
-})
+        r"/api/*": {"origins": allowed_origin},  # API modules: authors, books, users, etc.
+        r"/stream/*": {"origins": allowed_origin},  # Media streaming endpoints
+        r"/files/*": {"origins": allowed_origin},  # File-serving routes
+        r"/download/*": {"origins": allowed_origin},  # File download endpoints
+        r"/login*": {"origins": allowed_origin},  # Authentication: login, OTP handling
+        r"/api/admin/*": {"origins": allowed_origin},  # Admin-related endpoints
+    })
