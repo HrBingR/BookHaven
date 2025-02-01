e4b9eb20aa20e2c7235e903979483b57a451bdde
anthonysmillie <anthony@everlytic.com>
2025-02-01 21:52:33 +0200

    Wrapping app in custom config provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ConfigProvider } from './context/ConfigProvider';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <ConfigProvider>
+            <App />
+        </ConfigProvider>
+    </StrictMode>,
 )
