ad3793e694d78e03c5a3a86c91230313eea60544
anthonysmillie <anthony@everlytic.com>
2025-02-02 20:19:30 +0200

    Extending bootstrap theme colors

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import "./styles/custom-bootstrap.scss"
 import App from './App.tsx'
 import { ConfigProvider } from './context/ConfigProvider';
 
