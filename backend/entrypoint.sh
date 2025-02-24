417f84a6c502fc5905616284e05ee13dd777603a
anthonysmillie <anthony@everlytic.com>
2025-02-24 02:35:39 +0200

    Fixed Celery log levels

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -6,7 +6,7 @@ APP_PORT="${PORT:-5000}"
 ENABLE_HTTPS="${ENABLE_HTTPS:-false}"
 SSL_CERT_FILE="/ssl/${SSL_CERT_FILE:-}"
 SSL_KEY_FILE="/ssl/${SSL_KEY_FILE:-}"
-LOG_LEVEL="${LOG_LEVEL:-info}"
+CELERY_LOG_LEVEL="${CELERY_LOG_LEVEL:-info}"
 
 echo "Checking and applying database migrations..."
 if ! python migrations.py; then
@@ -28,7 +28,7 @@ else
 fi
 
 echo "Starting Celery and Celery Beat..."
-celery -A celery_app.celery worker --loglevel="$LOG_LEVEL" &
-celery -A celery_app.celery beat --loglevel="$LOG_LEVEL" &
+celery -A celery_app.celery worker --loglevel="$CELERY_LOG_LEVEL" &
+celery -A celery_app.celery beat --loglevel="$CELERY_LOG_LEVEL" &
 
 wait
