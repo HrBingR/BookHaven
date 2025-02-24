6d2549dda12cdfdde0cbb6356a2fe59512e21e84
anthonysmillie <anthony@everlytic.com>
2025-02-24 02:33:22 +0200

    Fixed some setting defaults and added OPDS settings

--- backend/config/config.py
+++ backend/config/config.py
@@ -30,27 +30,29 @@ class Config:
         self.SECRET_KEY = os.getenv('SECRET_KEY', "").strip()
         self.ADMIN_PASS = os.getenv('ADMIN_PASS')
         self.ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
-        self.ADMIN_RESET = str_to_bool(os.getenv('ADMIN_RESET', "false"))
-        self.CF_ACCESS_AUTH = str_to_bool(os.getenv('CF_ACCESS_AUTH', "false"))
-        self.ALLOW_UNAUTHENTICATED = str_to_bool(os.getenv('ALLOW_UNAUTHENTICATED', "false"))
-        self.WRITE_TO_EPUB = str_to_bool(os.getenv('WRITE_TO_EPUB', "false"))
+        self.ADMIN_RESET = str_to_bool(os.getenv('ADMIN_RESET', False))
+        self.CF_ACCESS_AUTH = str_to_bool(os.getenv('CF_ACCESS_AUTH', False))
+        self.ALLOW_UNAUTHENTICATED = str_to_bool(os.getenv('ALLOW_UNAUTHENTICATED', False))
+        self.WRITE_TO_EPUB = str_to_bool(os.getenv('WRITE_TO_EPUB', False))
 
-        self.OIDC_ENABLED = str_to_bool(os.getenv('OIDC_ENABLED', "false"))
+        self.OIDC_ENABLED = str_to_bool(os.getenv('OIDC_ENABLED', False))
         self.OIDC_CLIENT_ID = os.getenv('OIDC_CLIENT_ID', None)
         self.OIDC_CLIENT_SECRET = os.getenv('OIDC_CLIENT_SECRET', None)
         self.OIDC_PROVIDER = os.getenv('OIDC_PROVIDER', None)
         self.OIDC_METADATA_ENDPOINT = os.getenv('OIDC_METADATA_ENDPOINT', None)
-        self.OIDC_AUTO_REGISTER_USER = str_to_bool(os.getenv('OIDC_AUTO_REGISTER_USER', 'false'))
-        self.OIDC_AUTO_LINK_USER = str_to_bool(os.getenv('OIDC_AUTO_LINK_USER', 'false'))
+        self.OIDC_AUTO_REGISTER_USER = str_to_bool(os.getenv('OIDC_AUTO_REGISTER_USER', False))
+        self.OIDC_AUTO_LINK_USER = str_to_bool(os.getenv('OIDC_AUTO_LINK_USER', False))
 
         self.REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
         self.REDIS_PORT = os.getenv('REDIS_PORT', '6379')
         self.REDIS_PASSWORD = os.getenv('REDIS_PASSWORD', "").strip()
         self.REDIS_LIMITER_DB = os.getenv('REDIS_LIMITER_DB', 0)
         self.REDIS_SCHEDULER_DB = os.getenv('REDIS_SCHEDULER_DB', 5)
+        self.REDIS_OPDS_DB = os.getenv('REDIS_OPDS_DB', 8)
 
         self.RATE_LIMITER_ENABLED = str_to_bool(os.getenv('RATE_LIMITER_ENABLED', True))
         self.SCHEDULER_ENABLED = str_to_bool(os.getenv('SCHEDULER_ENABLED', True))
+        self.OPDS_ENABLED = str_to_bool(os.getenv('OPDS_ENABLED', False))
 
         self.PERIODIC_SCAN_INTERVAL = os.getenv('PERIODIC_SCAN_INTERVAL', 10)
 
@@ -78,5 +80,12 @@ class Config:
         if not self.REDIS_PASSWORD:
             return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_SCHEDULER_DB}"
         return f"redis://:{self.REDIS_PASSWORD}@{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_SCHEDULER_DB}"
+
+    @property
+    def OPDS_REDIS_URI(self):
+        if not self.REDIS_PASSWORD:
+            return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_OPDS_DB}"
+        return f"redis://:{self.REDIS_PASSWORD}@{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_OPDS_DB}"
+
 
 config = Config()
