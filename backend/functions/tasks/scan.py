48acf8b92124b3cbe05de2776fae3589948ed07e
anthonysmillie <anthony@everlytic.com>
2025-06-14 14:54:37 +0200

    Added redis locking to ensure only one scan runs at a time regardless of workers and scheduling.

--- backend/functions/tasks/scan.py
+++ backend/functions/tasks/scan.py
@@ -1,21 +1,32 @@
+from redis import Redis
 from celery_app import celery
-from functions.metadata.scan import scan_and_store_metadata
-from config.config import config
-from sqlalchemy.exc import SQLAlchemyError
 from celery.exceptions import MaxRetriesExceededError
+from config.config import config
 from config.logger import logger
+from sqlalchemy.exc import SQLAlchemyError
+from functions.metadata.scan import scan_and_store_metadata
 
-@celery.task(bind=True, max_retries=5)
+logger.info(f"REDIS LOCK URI: {config.REDIS_LOCK_DB_URI}")
+redis_lock_client = Redis.from_url(config.REDIS_LOCK_DB_URI)
+
+@celery.task(bind=True, name="scan_library_task", max_retries=5)
 def scan_library_task(self):
+    lock = redis_lock_client.lock("scan_lock", timeout=900)  # e.g., 15-minute hard max timeout
+    if not lock.acquire(blocking=False):
+        logger.warning("Another scan is already running. Skipping this one.")
+        return "scan_already_running"
+
     try:
+        logger.info("Starting scan with lock acquired.")
         scan_and_store_metadata(config.BASE_DIRECTORY)
+        return "scan_completed"
     except SQLAlchemyError as exc:
         if self.request.retries >= self.max_retries:
-            logger.exception(
-                f"Maximum retries exceeded for task '{self.name}' after {self.request.retries} attempts. "
-                f"Original DB Error: {str(exc)}"
-            )
+            logger.exception(f"Max retries exceeded after {self.request.retries} attempts. DB Error: {str(exc)}")
             raise MaxRetriesExceededError()
         retry_delay = 3 ** self.request.retries
-        logger.warning(f"Retrying task due to database error: {str(exc)}. Attempt {self.request.retries + 1}")
+        logger.warning(f"Retrying due to DB error: {str(exc)} (attempt {self.request.retries + 1})")
         raise self.retry(exc=exc, countdown=retry_delay)
+    finally:
+        lock.release()
+        logger.info("Scan complete. Lock released.")
