89521eb38e101c4fe7560ed9fe8c76f95298ba19
anthonysmillie <anthony@everlytic.com>
2025-06-25 02:01:02 +0200

    Changed from gunicorn sync to gthread to better support file uploads, increased timeout

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -17,14 +17,14 @@ fi
 if [ "$ENABLE_HTTPS" = "true" ]; then
     if [ -f "$SSL_CERT_FILE" ] && [ -f "$SSL_KEY_FILE" ]; then
         echo "Starting $NUM_WORKERS gunicorn workers with HTTPS..."
-        gunicorn -w 1 --certfile="$SSL_CERT_FILE" --keyfile="$SSL_KEY_FILE" --config gunicorn_logging.py -b 0.0.0.0:"$APP_PORT" main:app &
+        gunicorn -w 1 --worker-class gthread --certfile="$SSL_CERT_FILE" --keyfile="$SSL_KEY_FILE" --timeout 300 --config gunicorn_logging.py -b 0.0.0.0:"$APP_PORT" main:app &
     else
         echo "ERROR: HTTPS is enabled but the SSL_CERT_FILE or SSL_KEY_FILE is missing. Exiting..."
         exit 1
     fi
 else
     echo "Starting $NUM_WORKERS gunicorn workers without HTTPS..."
-    gunicorn -w 1 --config gunicorn_logging.py -b 0.0.0.0:"$APP_PORT" main:app &
+    gunicorn -w 1 --timeout 300 --worker-class gthread --config gunicorn_logging.py -b 0.0.0.0:"$APP_PORT" main:app &
 fi
 
 echo "Starting Celery and Celery Beat..."
